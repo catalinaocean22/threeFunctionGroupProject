9e82136a078db605518f44169ba4819a9077c632
Kevin Dunn <kcdunn8@gmail.com>
2022-11-21 23:48:02 -0800

    Creating files and functions. Starting on conv_num functionality

---
+++ task.py
@@ -0,0 +1,26 @@
+import re
+
+
+def conv_num(num_str):
+
+    # If the input is not a string, is an empty string, or contains multiple '.' characters, return None
+    multiple_decimals = re.match(r"\.+.*\.+", num_str)
+    if type(num_str) != str or len(num_str) == 0 or multiple_decimals:
+        return None
+
+    int_regex = r"^-?[0-9]+$"
+    float_regex = r"^-?[0-9]+\.[0-9]+"
+
+    if re.fullmatch(int_regex, num_str):
+        return int(num_str)
+
+    if re.fullmatch(float_regex, num_str):
+        return float(num_str)
+
+
+def my_datetime(num_sec):
+    pass
+
+
+def conv_endian(num, endian='big'):
+    pass
--- tests.py
+++ tests.py
@@ -1,10 +1,32 @@
 import unittest
+from task import conv_num, my_datetime, conv_endian
 
 
-class TestCase(unittest.TestCase):
+class TestConvNum(unittest.TestCase):
 
-    def test1(self):
-        self.assertTrue(True)
+    def test_example_1(self):
+        self.assertEqual(conv_num('12345'), 12345)
+
+    def test_example_2(self):
+        self.assertEqual(conv_num('-123.45'), -123.45)
+
+    def test_example_3(self):
+        self.assertEqual(conv_num('.45'), 0.45)
+
+    def test_example_4(self):
+        self.assertEqual(conv_num('123.'), 123.0)
+
+    def test_example_5(self):
+        self.assertEqual(conv_num('0xAD4'), 2772)
+
+    def test_example_6(self):
+        self.assertIsNone(conv_num('0xAZ4'))
+
+    def test_example_7(self):
+        self.assertIsNone(conv_num('12345A'))
+
+    def test_example_8(self):
+        self.assertIsNone(conv_num('12.3.45'))
 
 
 if __name__ == '__main__':
