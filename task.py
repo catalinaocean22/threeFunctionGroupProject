24b0baad8c9e80bc45443c76dfe2fd19f3d9a0ba
ballaib <ballaib@oregonstate.edu>
2022-11-25 23:49:02 -0500

    fixed flake8 complexity error

--- task.py
+++ task.py
@@ -45,57 +45,69 @@ def conv_num(num_str):
     if type(num_str) != str or len(num_str) == 0 or multiple_decimals:
         return None
 
-    # Use this to check if num_string is valid hex
+    # check if hex
     hex_regex = r"^-?(0x|0X)+[0-9a-fA-F]+$"
     if re.fullmatch(hex_regex, num_str):
-        return_val = 0
-        exponent = 0
-        positive = True
-        # Check if the number is negative, and remove the symbol if so
-        if num_str[0] == '-':
-            positive = False
-            num_str = num_str[1:]
+        return conv_hex(num_str)
 
-        # Remove the hex prefix and uppercase the string for case insensitivity
-        num_str = num_str[2:].upper()
-
-        # Iterate through the num_str backwards,
-        # increasing the exponent each time
-        for hex_str in reversed(num_str):
-            return_val = return_val + (char_to_digit[hex_str] * 16 ** exponent)
-            exponent += 1
-        if positive:
-            return return_val
-        else:
-            return return_val * -1
-
-    # Use this to check if num_str is a valid number
+    # check if decimal
     num_regex = r"^-?[0-9]*\.?[0-9]*$"
-
     if re.fullmatch(num_regex, num_str):
-        return_val = 0
-        exponent = 0
-        positive = True
+        return conv_decimal(num_str)
 
-        # Check if the number is negative, and remove the symbol if so
-        if num_str[0] == '-':
-            positive = False
-            num_str = num_str[1:]
-
-        # Iterate through the num_str backwards,
-        # increasing the exponent each time
-        for num in reversed(num_str):
-            if num == '.':
-                return_val = return_val / 10 ** exponent
-                exponent = 0
-                continue
-            return_val = return_val + (char_to_digit[num]) * 10 ** exponent
-            exponent += 1
-        if positive:
-            return return_val
-        else:
-            return return_val * -1
+    # invalid input
     return None
+
+
+def conv_hex(num_str):
+    # setup
+    return_val = 0
+    exponent = 0
+    positive = True
+
+    # Check if the number is negative, and remove the symbol if so
+    if num_str[0] == '-':
+        positive = False
+        num_str = num_str[1:]
+
+    # Remove the hex prefix and uppercase the string for case insensitivity
+    num_str = num_str[2:].upper()
+
+    # Iterate through the num_str backwards,
+    # increasing the exponent each time
+    for hex_str in reversed(num_str):
+        return_val = return_val + (char_to_digit[hex_str] * 16 ** exponent)
+        exponent += 1
+    if positive:
+        return return_val
+    else:
+        return return_val * -1
+
+
+def conv_decimal(num_str):
+    # setup
+    return_val = 0
+    exponent = 0
+    positive = True
+
+    # Check if the number is negative, and remove the symbol if so
+    if num_str[0] == '-':
+        positive = False
+        num_str = num_str[1:]
+
+    # Iterate through the num_str backwards,
+    # increasing the exponent each time
+    for num in reversed(num_str):
+        if num == '.':
+            return_val = return_val / 10 ** exponent
+            exponent = 0
+            continue
+        return_val = return_val + (char_to_digit[num]) * 10 ** exponent
+        exponent += 1
+    if positive:
+        return return_val
+    else:
+        return return_val * -1
 
 
 def my_datetime(num_sec):
